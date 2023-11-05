a4419d47c01caa39491adabef972e5d383fa8c51
JesraAli <jesra.ali@gmail.com>
2023-11-05 02:00:49 +0000

    Add text input, clean up code structure

--- hn23/src/App.js
+++ hn23/src/App.js
@@ -1,75 +1,99 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './index.css';
 
+function typingEffect(recievedText, delay) {
+  const [text, setText] = useState('');
+  const [isTyping, setIsTyping] = useState(true);
 
-function gameDisplay(user_input) {
+  useEffect(() => {
+    let currentIndex = 0;
 
-  //Check what game was inputted by user
-  //Call specific function to draw ASCII game
+    if (isTyping) {
+      const timer = setInterval(() => {
+        if (currentIndex < recievedText.length) {
+          setText(recievedText.slice(0, currentIndex + 1));
+          currentIndex++;
+        }
+        else {
+          setIsTyping(false);
+          clearInterval(timer);
+        }
+      }, delay);
 
-  //Have a continuous calling of this function?  To constantly update the game,
+      return () => {
+        clearInterval(timer);
+      };
+    }
+  }, [isTyping, recievedText, delay]);
 
   return (
-    <div>
-      {/* Display ASCII game */}
-
-    </div>
-  )
+    { text, isTyping }
+  );
 }
 
-function chatProcessing(user_input) {
-
-  //Retrieve input user responses, process information
-  //Display returned output data
-
+function TypingEffectComponent({ initialText }) {
+  const { text, isTyping } = typingEffect(initialText, 40);
   return (
     <div>
-
+      {text}
     </div>
   );
 }
 
-
 function App() {
-  // class App extends Component {
-  // render() {
+
+  const [string, setString] = useState('');
+
+  const updateString = (event) => {
+    setString(event.target.value);
+  }
+
+  const handleSubmit = (event) => {
+    event.preventDefault();  //Prevents page from refreshing
+    processResponse(string);
+    setString('');
+  }
+
   return (
     <div>
       <header>
-        {/* <img src={require('./hacknottsLogo.png')} style={{
-          width: '200px',
-          height: '200px',
-        }}></img> */}
-        <h1 style={{ margin:'auto' }}>HackNotts '23 </h1>
+
+        <h1 style={{ margin: 'auto' }}>HackNotts '23 </h1>
       </header>
 
       <main>
         <section className="terminalSection" style={{ marginTop: '50px' }}>
           <div className="computerScreen">
             <div className="buttonScreen" style={{ marginTop: '30px' }}>
-              {/* <button className="computerButton" onClick={()}></button> */}
-              {/* <button className="computerButton"></button> */}
 
               <div className="terminal" style={{ marginTop: '20px' }}>
-                <p>ADVENTURE GAME STUFF</p>
+                <TypingEffectComponent initialText="You find yourself in a dimly illuminated parking lot, rain pouring down as you peer through the car's window.
+                Recalling your long-standing curiosity about the rarely-visited arcade, you decide to finally explore it for yourself.">
+                </TypingEffectComponent>
                 <button className="computerButton"></button>
-
               </div>
+
+              <div>
+                <form onSubmit={handleSubmit}>
+                  <input
+                    style={{ fontSize: 14 }}
+                    className="inputBar"
+                    type='text'
+                    onChange={(event) => updateString(event)}
+                    placeholder='|'
+                  />
+                  <button type="submit" style={{ display: 'none' }}>Submit</button>
+                </form>
+              </div>
+
             </div>
             <div className="computerLine"></div>
           </div>
 
         </section>
-
-        {/* <section classame="chatBar">
-          {chatProcessing()};
-        </section> */}
       </main>
-
     </div>
-
   )
 }
-// }
 
 export default App;
