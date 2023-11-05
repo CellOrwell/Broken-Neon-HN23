b7f333f47816c9d132b7043eb5f6f8d0e1ca23c4
JesraAli <jesra.ali@gmail.com>
2023-11-05 03:37:57 +0000

    Implement audio playing within terminal

--- hn23/src/App.js
+++ hn23/src/App.js
@@ -1,5 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import './index.css';
+import useSound from 'use-sound';
+import rainSound from './rain.mp3';
+import carSound from './carArriving.mp3';
+import doorSound from './doorSlam.mp3';
+import footstepsSound from './footsteps.mp3';
+
+
 
 function typingEffect(recievedText, delay) {
   const [text, setText] = useState('');
@@ -50,9 +57,38 @@ function App() {
 
   const handleSubmit = (event) => {
     event.preventDefault();  //Prevents page from refreshing
-    processResponse(string);
+    // processResponse(string);
     setString('');
   }
+
+  const [playRain] = useSound(rainSound);
+  const [playCar] = useSound(carSound);
+  const [playDoor] = useSound(doorSound);
+  const [playFootsteps] = useSound(footstepsSound);
+
+  const [soundIndex, setSoundIndex] = useState(1);
+
+
+  const handleInputClick = () => {
+    switch (soundIndex) {
+      case 1:
+        playRain();
+        break;
+      case 2:
+        playCar();
+        break;
+      case 3:
+        playDoor();
+        break;
+      case 4:
+        playFootsteps();
+        break;
+      default:
+        break;
+    }
+
+    setSoundIndex(soundIndex + 1);
+  };
 
   return (
     <div>
@@ -67,9 +103,11 @@ function App() {
             <div className="buttonScreen" style={{ marginTop: '30px' }}>
 
               <div className="terminal" style={{ marginTop: '20px' }}>
+
                 <TypingEffectComponent initialText="You find yourself in a dimly illuminated parking lot, rain pouring down as you peer through the car's window.
                 Recalling your long-standing curiosity about the rarely-visited arcade, you decide to finally explore it for yourself.">
                 </TypingEffectComponent>
+
                 <button className="computerButton"></button>
               </div>
 
@@ -80,6 +118,9 @@ function App() {
                     className="inputBar"
                     type='text'
                     onChange={(event) => updateString(event)}
+                    // onClick={() => playRain()}
+                    onClick={handleInputClick}
+
                     placeholder='|'
                   />
                   <button type="submit" style={{ display: 'none' }}>Submit</button>
